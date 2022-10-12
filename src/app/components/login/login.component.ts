cb5c7735660e54fe81545a90ef1308745d24842a
Viktor Shchepliahin <viktor.scheplyagin@y-tree.com>
2022-10-12 19:33:32 +0200

    initial login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4)])
+    })
   }
 
+  submitLogin() {
+    this.authService.login(this.loginForm.value).subscribe({
+      next: () => this.router.navigate(['admin']),
+      error: (error) => window.alert(error.message)
+    })
+  }
+
+  get isEmailInvalid() {
+    return this.loginForm.get('email')?.dirty
+      && this.loginForm.get('email')?.invalid
+  }
+
+  get isPasswordInvalid() {
+    return this.loginForm.get('password')?.dirty
+      && this.loginForm.get('password')?.invalid
+  }
 }
