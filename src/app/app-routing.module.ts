c7c7f11982d33d24da91374f16a4a02c2608a981
Viktor Shchepliahin <viktor.scheplyagin@y-tree.com>
2022-10-12 19:31:45 +0200

    add root routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthGuard } from './guards/auth.guard';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    canDeactivate: [AuthGuard],
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then(
+        (module) => module.AdminModule
+      ),
+  },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
