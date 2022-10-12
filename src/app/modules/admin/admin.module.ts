f96a97af6f2bfbc7b9125580fb69d799231ac2f7
Viktor Shchepliahin <viktor.scheplyagin@y-tree.com>
2022-10-12 19:34:07 +0200

    all the stuff

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ContactsDetailsComponent } from './components/contacts-details/contacts-details.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactsComponent,
+    ContactsDetailsComponent,
+    AdminDashboardComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, of, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    localStorage.getItem('token');
+  }
+
+  get isLoggedIn() {
+    return this.getToken() !== undefined;
+  }
+
+  login(userInfo: {
+    email: string;
+    password: string;
+  }): Observable<never | boolean> {
+    const isEmailAuthOk = userInfo.email === 'a@b.c';
+    const isPasswordAuthOk = userInfo.password === 'aaaa';
+
+    if (isEmailAuthOk && isPasswordAuthOk) {
+      this.setToken('qwerty');
+      return of(true);
+    }
+
+    return throwError(() => new Error('login is failed'));
+  }
 }
